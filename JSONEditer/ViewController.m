8818194a92ba202ca12cf4ad81386bd7c9acc9fc
黄大卫 <huangdawei@meituan.com>
2017-12-29 19:59:58 +0800

    修改README

--- JSONEditer/ViewController.m
+++ JSONEditer/ViewController.m
@@ -27,7 +27,7 @@
 {
     HDWJsonEditerPage * editerPage = [[HDWJsonEditerPage alloc] init];
     [self presentViewController:editerPage animated:YES completion:^{
-        //
+        editerPage.jsonString = @"{'data':{'imgUrl':'https://p1.meituan.net/pmsicon/e8dd6c2ee72a40f0b40a3c21fad79e58.jpg','currentTime':1499342461100,'redirectUrl':'http://www.baidu.com','id':'101012991901'}}";
     }];
 }
 
